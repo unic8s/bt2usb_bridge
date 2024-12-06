45e869a9b5837daaf1688a568a48eee9968f30af
unic8s <erik.lembke@unic8.com>
2024-12-06 23:13:41 +0100

    Add BLE auto-scanning, add device name specific paring

--- bt2usb_keyboard.ino
+++ bt2usb_keyboard.ino
@@ -3,11 +3,14 @@
 #include "Keyboard.h"
 
 
-#define BT_USB_OFFSET 61
-#define KEY_ASCII_OFFET 13
+#define BT_USB_OFFSET     61
+#define KEY_ASCII_OFFET   13
+
+#define DEVICE_NAME       "HD2 Macropad"
 
 
 BluetoothHIDMaster hid;
+BluetoothHCI hci;
 
 
 uint8_t modifierMap[128] = {};
@@ -39,20 +42,83 @@ void kb(void *cbdata, int key) {
 }
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
   setupModifierMap();
 
   Serial.begin();
   delay(3000);
 
-  Serial.printf("Starting HID master, put your device in pairing mode now.\n");
+  Keyboard.begin();
 
   hid.onKeyDown(kb, (void *)true);
   hid.onKeyUp(kb, (void *)false);
 
-  hid.begin(true);
-  hid.connectBLE();
+  connectMacropad();
+}
 
-  Keyboard.begin();
+bool bleScanning = false;
+bool bleRescan = false;
+bool bleFound = false;
+unsigned long lastScan = 0;
+
+void connectMacropad(){
+  unsigned long now = millis();
+
+  if(bleFound || bleScanning || now - lastScan < 5000){
+    if(!bleFound && !hid.connected()){
+      bleRescan = true;
+    }
+
+    return;
+  }
+
+  bleScanning = true;
+  lastScan = now;
+
+  hid.end();
+
+  delay(1000);
+
+  hci.install();
+  hci.begin();
+
+  auto l = hci.scanBLE(BluetoothHCI::any_cod);
+
+  Serial.printf("%-8s | %-17s | %-4s | %s\n", "Class", "Address", "RSSI", "Name");
+  Serial.printf("%-8s | %-17s | %-4s | %s\n", "--------", "-----------------", "----", "----------------");
+
+  for (auto e : l) {
+    Serial.printf("%08lx | %17s | %4d | %s\n", e.deviceClass(), e.addressString(), e.rssi(), e.name());
+
+    String deviceName = e.name();
+
+    if(deviceName == DEVICE_NAME){
+      bleFound = true;
+      bleRescan = false;
+
+      hci.scanFree();
+      hci.uninstall();
+
+      delay(1000);
+
+      hid.begin(true);
+      hid.connectBLE(e.address(), e.addressType());
+
+      Serial.println("Device connected");
+      
+      bleScanning = false;
+      return;
+    }
+  }
+
+  Serial.println("No device found");
+
+  if(!hid.connected()){
+    bleRescan = true;
+  }
+  
+  bleScanning = false;
 }
 
 void loop() {
@@ -61,21 +127,26 @@ void loop() {
       delay(1);
     }
 
+    Serial.println("Cleanup after reset");
+
+    bleFound = false;
+    digitalWrite(LED_BUILTIN, LOW);
+
     hid.disconnect();
     hid.clearPairing();
 
-    Serial.printf("Restarting HID master, put your device in pairing mode now.\n");
+    delay(1000);
 
-    hid.connectBLE();
+    Serial.println("Rescanning");
+
+    connectMacropad();
   }
-}
 
-void setup1() {
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+  if(!hid.connected() || bleRescan){
+    connectMacropad();
+  }
 
-void loop1() {
-  digitalWrite(LED_BUILTIN, hid.connected() ? HIGH : LOW);
+  digitalWrite(LED_BUILTIN, bleFound ? HIGH : LOW);
 
   yield();
   delay(1);
